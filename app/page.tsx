ce6efcf2e38116708cbf306e4d307e89f9560404
bleartdemaku <bd47165@ubt-uni.net>
2023-12-01 20:39:25 +0100

    added components and public folder

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
-import Image from 'next/image'
+
 
 export default function Home() {
   return (
-    <h1 className="text-bold text-teal-500">Hello</h1>
+    <h1 className="font-bold text-teal-500 text-2xl">
+      Hello
+    </h1>
   )
 }
